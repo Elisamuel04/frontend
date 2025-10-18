3e476cda803b9f03564450ea7940bc11c6e3e27d
Elisamuel Valera Socarrás <98656922+Elisamuel04@users.noreply.github.com>
2025-10-18 07:33:16 -0500

    persistencia de datos
    mostrar en el frontend los datos guardados y así poder hacer CRUD hacia el backend

--- MiniKanban/src/app/services/kanban.service.ts
+++ MiniKanban/src/app/services/kanban.service.ts
@@ -11,7 +11,21 @@ export class KanbanService {
 
   constructor(private http: HttpClient) {}
 
+  
+  getItems(): Observable<KanbanItem[]> {
+    return this.http.get<KanbanItem[]>(this.baseUrl);
+  }
+
   saveItems(items: KanbanItem[]): Observable<any> {
     return this.http.post<any>(`${this.baseUrl}/bulk`, { items });
+  }
+
+  
+  updateItem(id: string, item: Partial<KanbanItem>): Observable<KanbanItem> {
+    return this.http.put<KanbanItem>(`${this.baseUrl}/${id}`, item);
+  }
+
+  deleteItem(id: string): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/${id}`);
   }
 }
