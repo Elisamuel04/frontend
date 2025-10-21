f90dab2b5bb6ae7516c0a104dc5ef2a06948cfbd
Elisamuel Valera Socarrás <98656922+Elisamuel04@users.noreply.github.com>
2025-10-21 14:29:16 -0500

    Frontend envie el token al baackend

--- MiniKanban/src/app/services/kanban.service.ts
+++ MiniKanban/src/app/services/kanban.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { KanbanItem } from '../interfaces/kanban.interface';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root',
@@ -9,23 +10,32 @@ import { KanbanItem } from '../interfaces/kanban.interface';
 export class KanbanService {
   private baseUrl = 'http://localhost:3000/api/tasks';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private auth: AuthService) {}
+
+  private getAuthHeaders(): HttpHeaders {
+    const token = this.auth.getToken();
+    return new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    });
+  }
 
   
+
   getItems(): Observable<KanbanItem[]> {
-    return this.http.get<KanbanItem[]>(this.baseUrl);
+    return this.http.get<KanbanItem[]>(this.baseUrl, { headers: this.getAuthHeaders() });
   }
 
   saveItems(items: KanbanItem[]): Observable<any> {
-    return this.http.post<any>(`${this.baseUrl}/bulk`, { items });
+    return this.http.post<any>(`${this.baseUrl}/bulk`, { items }, { headers: this.getAuthHeaders() });
   }
 
   
   updateItem(id: string, item: Partial<KanbanItem>): Observable<KanbanItem> {
-    return this.http.put<KanbanItem>(`${this.baseUrl}/${id}`, item);
+    return this.http.put<KanbanItem>(`${this.baseUrl}/${id}`, item, { headers: this.getAuthHeaders() });
   }
 
   deleteItem(id: string): Observable<void> {
-    return this.http.delete<void>(`${this.baseUrl}/${id}`);
+    return this.http.delete<void>(`${this.baseUrl}/${id}`, { headers: this.getAuthHeaders() });
   }
 }
